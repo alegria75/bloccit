e4a8fb3d8edd8e00a7dba789623818052a465e8c
vagrant <vagrant@rails-dev-box>
2013-11-24 21:37:43 +0000

    Ajaxified comment destruction

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+	respond_to :html, :js
 	def create
 	 @post = Post.find(params[:post_id])
 	 @topic = @post.topic
@@ -12,23 +13,22 @@ class CommentsController < ApplicationController
 	end
 
 		def destroy
-		@topic = Topic.find(params[:topic_id]) # change get topic from post
-				@post = @topic.posts.find(params[:post_id]) #change
+			@topic = Topic.find(params[:topic_id]) # change get topic from post
+			@post = @topic.posts.find(params[:post_id]) #change
 
-				@comment = @post.comments.find(params[:id])
-
-				authorize! :destroy, @comment, message: "You need to own the comment to delete it."
-				if @comment.destroy
-						flash[:notice] = "Comment was removed"
-						redirect_to [@topic, @post]
-				else
-						flash[:error] = "Comment couldn't be deleted. Try again."
-						redirect_to [@topic, @post]
-				end
-		end
-
+			@comment = @post.comments.find(params[:id])
+			authorize! :destroy, @comment, message: "You need to own the comment to delete it."
+			
+			if @comment.destroy
+				flash[:notice] = "Comment was removed"
+			else
+				flash[:error] = "Comment couldn't be deleted. Try again."
+			end
 		
-
+		respond_with(@comment) do |f|
+			f.html { redirect_to[@topic, @post] }
+		end
+		
 		# def comment_params
 		# 		params.require(:comment).permit(:commenter, :body, :post_id)
 		# end
